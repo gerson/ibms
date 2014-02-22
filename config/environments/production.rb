7331c8642c6e3ef3187e8ee3caf412841063eac2
Gerson Villanueva <faillacef@gmail.com>
2014-02-22 03:23:45 -0500

    updating setting for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,9 @@ Ibms::Application.configure do
       :address => "smtp.gmail.com",
       :port => 587,
       :domain => "gmail.com",
-      :authentication => :login,
       :user_name => ENV['GMAIL_USERNAME'],
-      :password => ENV['GMAIL_PASSWORD']
+      :password => ENV['GMAIL_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
   }
 end
